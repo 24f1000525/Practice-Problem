31138121c2344fecaf204060431fb9a89d9174a1
Prasun Kumar <24f1000525@ds.study.iitm.ac.in>
2026-07-12 23:51:29 +0530

    Time: 8 ms (63.56%), Space: 169.5 MB (35.94%) - LeetHub

---
+++ LeetCode/Medium/3987-minimum-total-cost-to-process-all-elements/3987-minimum-total-cost-to-process-all-elements.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int minimumCost(vector<int>& nums, int k) {
+        long long tcost=0;
+        long long itr=0;
+        long long rem=k;
+        const long long MOD = 1e9 + 7;
+        
+        for(int i=0;i<nums.size();i++){
+            if(rem>=nums[i]){
+                rem=rem-nums[i];
+            }
+            else{
+                long long need = nums[i] - rem;
+                long long cnt = (need + k - 1) / k;
+
+                __int128 add = (__int128)(2 * itr + cnt + 1) * cnt / 2;
+                tcost = (tcost + (long long)(add % MOD)) % MOD;
+
+                itr += cnt;
+                rem += cnt * k;
+                rem -= nums[i];
+
+            }
+            
+        }
+        return tcost;
+    }
+};
